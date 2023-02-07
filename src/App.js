850c5cd87ce037957026744ff6b45bcbb44fc307
G808 <hucciallstars@gmail.com>
2023-02-07 19:43:45 +0200

    Minor css edits

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
     <div className='container mt-3 d-flex justify-content-center min-vh-100 align-items-center'>
       <div className='row'>
         {/* Left side form */}
-        <div className='col-12 col-md-6 px-5'>
+        <div className='form col-12 col-md-6'>
           <SignUp />
         </div>
         {/* Image */}
-        <div className='col-12 col-md-6 d-flex flex-wrap align-items-center px-5'>
+        <div className='image col-12 col-md-6 d-flex flex-wrap align-items-center'>
           <img className='image w-80' src={regImg} alt='register form' />
         </div>
       </div>
